ab52a7dfb8827b7d82db8610d7bf073ea40b118d
Cuong Nguyen <6370@ait.nsw.edu.au>
2018-05-14 17:29:34 +1000

    connection info

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 "# web" 
+Connection:
+206.189.152.236
+DB access:
+206.189.152.236/phpmyadmin
+senpai/thisisaverycomplexpassword
+
+file upload: sftp://206.189.152.236 
---
+++ login.php
@@ -0,0 +1,35 @@
+<?php
+SESSION_START();
+$error=''; // Variable To Store Error Message
+if (isset($_POST['submit'])) {
+if (empty($_POST['loginUserName']) || empty($_POST['loginUserPassword'])) {
+$error = "Username or Password is invalid";
+}
+else
+{
+// Define $username and $password
+$username=$_POST['loginUserName'];
+$password=$_POST['loginUserPassword'];
+// Establishing Connection with Server by passing server_name, user_id and password as a parameter
+$connection = mysql_connect("localhost", "access", "onetwothreefour5");
+// To protect MySQL injection for Security purpose
+$username = stripslashes($username);
+$password = stripslashes($password);
+$username = mysql_real_escape_string($username);
+$password = mysql_real_escape_string($password);
+$password = md5($password);
+// Selecting Database
+$db = mysql_select_db("shop", $connection);
+// SQL query to fetch information of registerd users and finds user match.
+$query = mysql_query("select * from USERINFO where USER_PASSWORD='$password' AND USER_NAME='$username'", $connection);
+$rows = mysql_num_rows($query);
+if ($rows == 1) {
+$_SESSION['login_user']=$username; // Initializing Session
+header("location: user.php"); // Redirecting To Other Page
+} else {
+$error = "Username or Password is invalid";
+}
+mysql_close($connection); // Closing Connection
+}
+}
+?>
