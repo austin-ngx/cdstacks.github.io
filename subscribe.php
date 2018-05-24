daf039d81d584dd825b9554ae2b344569b2703b6
Paulo Marchese <paulohenriquerm@hotmail.com>
2018-05-24 20:27:32 +1000

    Added Subscription functionality to Index.

---
+++ subscribe.php
@@ -0,0 +1,19 @@
+<?php
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+
+	include 'connection.php';
+	if(isset($_POST['email'])){
+		$sql = "INSERT INTO SUBSCRIPTIONS (ID, EMAIL) VALUES (NULL, '".$_POST['email']."')";
+		echo $sql;
+		$results = mysqli_query($conn, $sql);
+		echo $results;
+	}
+	else{
+		$sql = "INSERT INTO SUBSCRIPTIONS (ID, EMAIL) VALUES (NULL, '".$_GET['email']."')";
+		echo $sql;
+		$results = mysqli_query($conn, $sql);
+		echo $results;
+	}
+?>
